8ff777ead2a16b03d82f1ffadf48054f174f517b
Dumitru Strugariu <sstrugariu.7@icloud.com>
2024-12-10 16:44:19 +0200

    Implementing features.

--- Symfony Elder Care Application/src/Controller/Frontend/DefaultController.php
+++ Symfony Elder Care Application/src/Controller/Frontend/DefaultController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Helper\BreadcrumbsHelper;
 
 class DefaultController extends AbstractController
 {
@@ -20,6 +21,91 @@ class DefaultController extends AbstractController
 
         return $this->render('frontend/pages/index.html.twig', [
             'page' => $page
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="app_about")
+     */
+    public function about(EntityManagerInterface $em, BreadcrumbsHelper $helper): Response
+    {
+        /** @var Page $page */
+        $page = $em->getRepository(Page::class)->findOneBy(['machineName' => 'about']);
+
+        return $this->render('frontend/pages/index.html.twig', [
+            'page' => $page,
+            'breadcrumbs' => $helper::ABOUTUS_BREADCRUMBS
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function contact(EntityManagerInterface $em, BreadcrumbsHelper $helper): Response
+    {
+        /** @var Page $page */
+        $page = $em->getRepository(Page::class)->findOneBy(['machineName' => 'contact']);
+
+        return $this->render('frontend/pages/index.html.twig', [
+            'page' => $page,
+            'breadcrumbs' => $helper::CONTACT_BREADCRUMBS
+
+        ]);
+    }
+
+    /**
+     * @Route("/services", name="app_services")
+     */
+    public function services(EntityManagerInterface $em, BreadcrumbsHelper $helper): Response
+    {
+        /** @var Page $page */
+        $page = $em->getRepository(Page::class)->findOneBy(['machineName' => 'services']);
+
+        return $this->render('frontend/pages/index.html.twig', [
+            'page' => $page,
+            'breadcrumbs' => $helper::SERVICES_BREADCRUMBS
+        ]);
+    }
+
+    /**
+     * @Route("/terms", name="app_terms")
+     */
+    public function terms(EntityManagerInterface $em, BreadcrumbsHelper $helper): Response
+    {
+        /** @var Page $page */
+        $page = $em->getRepository(Page::class)->findOneBy(['machineName' => 'terms']);
+
+        return $this->render('frontend/pages/legal.html.twig', [
+            'page' => $page,
+            'breadcrumbs' => $helper::TERMS_BREADCRUMBS
+        ]);
+    }
+
+    /**
+     * @Route("/policy", name="app_policy")
+     */
+    public function policy(EntityManagerInterface $em, BreadcrumbsHelper $helper): Response
+    {
+        /** @var Page $page */
+        $page = $em->getRepository(Page::class)->findOneBy(['machineName' => 'policy']);
+
+        return $this->render('frontend/pages/legal.html.twig', [
+            'page' => $page,
+            'breadcrumbs' => $helper::POLICY_BREADCRUMBS
+        ]);
+    }
+
+    /**
+     * @Route("/cookies", name="app_cookies")
+     */
+    public function cookies(EntityManagerInterface $em, BreadcrumbsHelper $helper): Response
+    {
+        /** @var Page $page */
+        $page = $em->getRepository(Page::class)->findOneBy(['machineName' => 'cookies']);
+
+        return $this->render('frontend/pages/legal.html.twig', [
+            'page' => $page,
+            'breadcrumbs' => $helper::COOKIES_BREADCRUMBS
         ]);
     }
 }
---
+++ Symfony Elder Care Application/src/Helper/BreadcrumbsHelper.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Helper;
+
+class BreadcrumbsHelper
+{
+    const ABOUTUS_BREADCRUMBS = [
+        [
+            'name' => 'Acasă',
+            'route' => 'app_homepage',
+            'params' => []
+        ],
+        [
+            'name' => 'Despre noi',
+            'route' => null,
+            'params' => []
+        ]
+    ];
+
+    const CONTACT_BREADCRUMBS = [
+        [
+            'name' => 'Acasă',
+            'route' => 'app_homepage',
+            'params' => []
+        ],
+        [
+            'name' => 'Contact',
+            'route' => null,
+            'params' => []
+        ]
+    ];
+
+    const SERVICES_BREADCRUMBS = [
+        [
+            'name' => 'Acasă',
+            'route' => 'app_homepage',
+            'params' => []
+        ],
+        [
+            'name' => 'Funcționalități',
+            'route' => null,
+            'params' => []
+        ]
+    ];
+
+    const TERMS_BREADCRUMBS = [
+        [
+            'name' => 'Acasă',
+            'route' => 'app_homepage',
+            'params' => []
+        ],
+        [
+            'name' => 'Termeni si conditii',
+            'route' => null,
+            'params' => []
+        ]
+    ];
+
+    const POLICY_BREADCRUMBS = [
+        [
+            'name' => 'Acasă',
+            'route' => 'app_homepage',
+            'params' => []
+        ],
+        [
+            'name' => 'Politica de confidentialitate',
+            'route' => null,
+            'params' => []
+        ]
+    ];
+
+    const COOKIES_BREADCRUMBS = [
+        [
+            'name' => 'Acasă',
+            'route' => 'app_homepage',
+            'params' => []
+        ],
+        [
+            'name' => 'Politica de cookies',
+            'route' => null,
+            'params' => []
+        ]
+    ];
+}
